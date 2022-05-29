270d912c32d2e6a2f456bfd5a4e9512d3e16bef5
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2022-05-29 13:41:26 +0700

    Update configuration file

--- config/subscriptions.php
+++ config/subscriptions.php
@@ -1,5 +1,30 @@
 <?php
-// config for Soap/LaravelSubscriptions
+
+declare(strict_types=1);
+
 return [
+
+    // Manage autoload migrations
+    'autoload_migrations' => true,
+
+    // Subscriptions Database Tables
+    'tables' => [
+
+        'plans' => 'plans',
+        'plan_features' => 'plan_features',
+        'plan_subscriptions' => 'plan_subscriptions',
+        'plan_subscription_usage' => 'plan_subscription_usage',
+
+    ],
+
+    // Subscriptions Models
+    'models' => [
+
+        'plan' => \Soap\LaravelSubscriptions\Models\Plan::class,
+        'plan_feature' => \Soap\LaravelSubscriptions\Models\PlanFeature::class,
+        'plan_subscription' => \Soap\LaravelSubscriptions\Models\PlanSubscription::class,
+        'plan_subscription_usage' => \Soap\LaravelSubscriptions\Models\PlanSubscriptionUsage::class,
+
+    ],
 
 ];
